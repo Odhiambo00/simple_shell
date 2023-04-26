db950a3d857bf974697ad5c20d25d2fec6eab5dd
odhiambo elias <odhiamboelias99@gmail.com>
2023-04-25 22:27:23 -0700

    headerfile.h

---
+++ headerfile.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+char **str_break(char *ln, char *dlm);
+void free_arr(char **arr);
+void echo(int insrt);
+void get_path(char **shll);
+void exec_cmd(char **shll);
+void prt_enve(void);
+void sgt_Handler(int sg_num);
+void _condns(char **shll, char *ln);
+
+#endif
