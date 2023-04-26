e0b83fb2a3c4608657c8d6ece63e9f1bd791c42e
odhiambo elias <odhiamboelias99@gmail.com>
2023-04-26 10:03:32 -0700

    updated my files

--- split_string.c
+++ split_string.c
@@ -10,18 +10,19 @@
 char **str_break(char *ln, char *dlm)
 {
 	char *Ptr = NULL, **shll = NULL;
-	size_t i = 0;
+	size_t i = 0, old_size = 0;
 
 	Ptr = strtok(ln, dlm);
 	while (Ptr)
 	{
-		shll = _rllc(shll, ((i + 1) * sizeof(char *)));
+		shll = _rllc(shll, old_size, ((i + 1) * sizeof(char *)));
 		shll[i] = strdp(Ptr);
 		Ptr = strtok(NULL, dlm);
 		i++;
+		old_size = i * sizeof(char *);
 	}
 
-	shll = _rllc(shll, ((i + 1) * sizeof(char *)));
+	shll = _rllc(shll, old_size, ((i + 1) * sizeof(char *)));
 	shll[i] = NULL;
 
 	return (shll);
