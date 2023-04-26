58d2c64b153153973cb23ce1bafc08e63da5ae47
odhiambo elias <odhiamboelias99@gmail.com>
2023-04-25 23:53:00 -0700

    updated eniron.c to include allowed functions only

--- environ.c
+++ environ.c
@@ -7,9 +7,22 @@
 void prt_enve(void)
 {
 	char **c = environ;
+	ssize_t i;
 
-	for (; *c; c++)
+	while (*c)
 	{
-		printf("%s\n", *c);
+		i = write(STDOUT_FILENO, *c, strlen(*c));
+		if (i == -1)
+		{
+			perror("write error");
+			_exit(EXIT_FAILURE);
+		}
+		i = write(STDOUT_FILENO, "\n", 1);
+		if (i == -1)
+		{
+			perror("write error");
+			_exit(EXIT_FAILURE);
+		}
+		i++
 	}
 }
