25dee44915bbe8db635622cb337f12c877de9086
odhiambo elias <odhiamboelias99@gmail.com>
2023-04-26 04:20:21 -0700

    updated split_string.c

---
+++ split_string.c
@@ -0,0 +1,28 @@
+#include "headerfile.h"
+
+/**
+ * str_break - function that parses a line of string
+ * @ln: line of string to be parsed
+ * @dlm: the delimiter that ln will be parsed with
+ * Return: array of elements that have been parsed
+ */
+
+char **str_break(char *ln, char *dlm)
+{
+	char *Ptr = NULL, **shll = NULL;
+	size_t i = 0;
+
+	Ptr = strtok(ln, dlm);
+	while (Ptr)
+	{
+		shll = realloc(shll, ((i + 1) * sizeof(char *)));
+		shll[i] = strdup(Ptr);
+		Ptr = strtok(NULL, dlm);
+		i++;
+	}
+
+	shll = realloc(shll, ((i + 1) * sizeof(char *)));
+	shll[i] = NULL;
+
+	return (shll);
+}
